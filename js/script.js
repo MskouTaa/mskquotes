1b059c48d7e0b6e0c0b6c5d0e6585cbf759f58f4
MskouTaa <mskoutaa.02@gmail.com>
2021-05-04 04:06:56 +0000

    fix js bug

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var total = 123;
 function randomImg(){
   var randomNumber = Math.floor(Math.random() * total) + 1;
-  var imgName = "Q" + randomNumber + ".png";
+  var imgName = "q" + randomNumber + ".png";
   document.getElementById("pic").src = "Q/" + imgName;
   document.getElementById("download").href = "Q/" + imgName;
 }
